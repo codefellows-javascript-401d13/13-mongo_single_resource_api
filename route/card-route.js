eaa0fcac7ce2816f7035072902457f2ddcb3be8b
Dana Kulp <dkulp23@gmail.com>
2017-03-01 15:19:30 -0800

    Set up router file and wrote POST route. Completed server file with mongoose details and middleware.

--- route/card-route.js
+++ route/card-route.js
@@ -1,5 +1,16 @@
 'use strict';
 
 const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
 const Card = require('../model/card.js');
-const mongoose = require('mongoose');
+const debug = require('debug')('card:card-route');
+
+const cardRouter = module.exports = new Router();
+
+cardRouter.post('/api/card', jsonParser, function(req, res, next) {
+  debug('POST: api/card');
+
+  new Card(req.body).save()
+  .then( card => res.json(card))
+  .catch(next);
+});
--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
+const cardRouter = require('./route/card-route.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/cardapp';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(cardRouter);
 
 app.listen(PORT, () => {
   debug(`Server's up on port: ${PORT}`);
